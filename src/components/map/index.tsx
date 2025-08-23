8a67d3372e2ac0ecf03ec49830444a003d043c0e
Çağan Seyrek <caganseyrek@outlook.com.tr>
2025-08-23 20:01:49 +0300

    Added data source attribution and license information

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -9,17 +9,22 @@ import { Marker } from "@/components/map/marker";
 
 import { dataCategories } from "@/data";
 
+const initialSelections = ["muzeler"];
+
 export default function Map() {
   return (
     <MapContainer center={[38.575, 27.125]} zoom={9} className="h-dvh w-full">
       <LayersControl position="topright" collapsed={false}>
         <TileLayer
-          attribution="&copy; <a href='http://osm.org'>OpenStreetMap</a> contributors"
+          attribution="&copy; <a href='http://osm.org'>OpenStreetMap</a> contributors. Veriler <a href='https://acikveri.bizizmir.com/tr'>Bizİzmir Açık Veri Portalı</a> üzerinden sağlanmaktadır ve <a href='https://acikveri.bizizmir.com/tr/license'>CC BY 4.0</a> lisansı altındadır."
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {dataCategories.map((category) => (
-          <LayersControl.Overlay key={category.key} name={category.title}>
+          <LayersControl.Overlay
+            key={category.key}
+            name={category.title}
+            checked={initialSelections.includes(category.key)}>
             <FeatureGroup>
               {category.data.map((location) => (
                 <Marker
@@ -34,4 +39,3 @@ export default function Map() {
     </MapContainer>
   );
 }
-
